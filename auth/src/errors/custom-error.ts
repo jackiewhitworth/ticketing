0c78d299ec4fc4ad3b08b7e9242ccbf444d82750
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-10-17 12:24:59 -0500

    Initial commit

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,13 @@
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+  
+  constructor(message: string) {
+    super(message);
+
+    //required when extending a built-in class
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  //not defining a method, but defining a method signature
+  abstract serializeErrors(): { message: string; field?: string }[];
+}
