f40bceb7aac8cce947ea046ba9690190b16ed4bf
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-12-08 18:19:44 -0600

    Added additional route testing

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'p'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email or password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password'
+    })
+    .expect(400);
+
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+});
