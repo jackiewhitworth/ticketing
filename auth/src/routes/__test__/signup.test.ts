3a3e62e4c79fb556840295bd8d176b7435791bf1
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-12-09 22:05:04 -0600

    Completed tests for signup route handler

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -25,7 +25,7 @@ it('returns a 400 with an invalid password', async () => {
   return request(app)
     .post('/api/users/signup')
     .send({
-      email: 'testtest.com',
+      email: 'test@test.com',
       password: 'p'
     })
     .expect(400);
@@ -46,3 +46,32 @@ it('returns a 400 with missing email or password', async () => {
     })
     .expect(400);
 });
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'test@test.com',
+    password: 'password'
+  })
+  .expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+  });
